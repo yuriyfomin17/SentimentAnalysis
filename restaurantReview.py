3c43ec4dffc1991d6980cf2b9a87c96dd028a4b9
yuriyfomin17 <fominyura2012@gmail.com>
2021-04-11 19:07:48 +0300

    Update

--- restaurantReview.py
+++ restaurantReview.py
@@ -3,6 +3,11 @@ import string
 import nltk
 from nltk.stem import WordNetLemmatizer
 import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from sklearn.feature_extraction.text import CountVectorizer
+from random import shuffle
+import re
+from sklearn.ensemble import RandomForestClassifier
 
 
 def dividing_reviews(parameter, dataset):
@@ -37,18 +42,33 @@ def filtering_Reviews(reviews):
     return final_words
 
 
-def lemmatizeWords(final_words):
+def lemmatizeWords_plotFreq_distribution(final_words):
     wn = WordNetLemmatizer()
     lem_words = []
     for word in final_words:
         word = wn.lemmatize(word)
         lem_words.append(word)
     # The frequency distribution of the words
-    freq_dis = nltk.FreqDist(lem_words)
-
+    freq_dist = nltk.FreqDist(lem_words)
     # Frequency Distribution Plot
-    plt.subplots(figsize=(20, 12))
-    freq_dis.plot(30)
+    # plt.subplots(figsize=(20, 12))
+    # freq_dist.plot(30)
+    return freq_dist
+
+
+def wordCloud(lem_words):
+    res = ' '.join([i for i in lem_words if not i.isdigit()])
+    plt.subplots(figsize=(16, 10))
+    wordcloud = WordCloud(
+        background_color='black',
+        max_words=100,
+        width=1400,
+        height=1200
+    ).generate(res)
+    plt.imshow(wordcloud)
+    plt.title('Reviews World Cloud(100 words)')
+    plt.axis('off')
+    plt.show()
 
 
 def restaurantReviews():
@@ -59,9 +79,34 @@ def restaurantReviews():
 
     negative_rev = dividing_reviews(0, dataset)
 
+    all_rev = positive_rev + negative_rev
+    shuffle(all_rev)
+
     final_words_pos = filtering_Reviews(positive_rev)
     final_words_neg = filtering_Reviews(negative_rev)
-    lemmatizeWords(final_words_pos)
+
+    # lemmatize words e.g. (cars, car => car) , (are, were, was => is)
+    lem_words_pos = lemmatizeWords_plotFreq_distribution(final_words_pos)
+    lem_words_neg = lemmatizeWords_plotFreq_distribution(final_words_neg)
+
+    # Creating the Bag of Words model
+    cv = CountVectorizer(max_features=2000)
+    corpus = []
+    stopwords = nltk.corpus.stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    for i in range(0, 1000): #as the data as 1000 data points
+        review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+        review = review.lower()
+        review = review.split()
+        review = [lemmatizer.lemmatize(word) for word in review if not word in set(stopwords)]
+        review = ' '.join(review)
+        corpus.append(review)
+
+    cv = CountVectorizer(max_features = 2000)
+    #the X and y
+    X = cv.fit_transform(corpus).toarray()
+    y = dataset.iloc[:, 1].values
+    print("Hello")
 
 
 if '__main__' == __name__:
